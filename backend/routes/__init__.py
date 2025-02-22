78dcadac343b16abebaccd3c15cb984a0e358b6e
YuanhuiXu <xuuuyuanhui@foxmail.com>
2025-02-22 22:26:01 +0000

    integrate external api calls

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -3,8 +3,10 @@ from flask import Blueprint
 # Import individual route modules
 from .chat import chat_bp
 from .user import user_bp
+from .service import service_bp
 
 # Define a function to register Blueprints
 def register_blueprints(app):
-    app.register_blueprint(chat_bp, url_prefix="/api")
     app.register_blueprint(user_bp)
+    app.register_blueprint(chat_bp, url_prefix="/api")
+    app.register_blueprint(service_bp, url_prefix="/api")
