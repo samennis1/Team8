aa7dae596d3a2ea7480939774423afe3460536ea
YuanhuiXu <xuuuyuanhui@foxmail.com>
2025-02-23 02:17:51 +0000

    product api calls

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -4,9 +4,11 @@ from flask import Blueprint
 from .chat import chat_bp
 from .user import user_bp
 from .service import service_bp
+from .product import product_bp
 
 # Define a function to register Blueprints
 def register_blueprints(app):
     app.register_blueprint(user_bp)
     app.register_blueprint(chat_bp, url_prefix="/api")
     app.register_blueprint(service_bp, url_prefix="/api")
+    app.register_blueprint(product_bp, url_prefix="/api")
---
+++ backend/routes/product.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from flask import Blueprint, jsonify, request
+from db_config import get_db
+from firebase_admin import firestore
+from utils import generate_otp_token
+
+product_bp = Blueprint("product", __name__)
+
+db = get_db()
+
+# Get products
+@product_bp.route("/products", methods=["GET"])
+def get_all_products():
+    products = []
+    product_docs = db.collection("product").get()
+    
+    for doc in product_docs:
+        product_data = doc.to_dict()
+        product_data['product_id'] = doc.id
+        products.append(product_data)
+    
+    return jsonify(products), 200
+
+# Get product by id
+@product_bp.route("/products/<product_id>", methods=["GET"])
+def get_product(product_id):
+    product_ref = db.collection("product").document(product_id)
+    product_doc = product_ref.get()
+    
+    if product_doc.exists:
+        return jsonify(product_doc.to_dict()), 200
+    else:
+        return jsonify({"error": f"Chat with id {product_id} not found"}), 404
+
+# Update product
+@product_bp.route("/products/<product_id>", methods=["PATCH"])
+def update_product(product_id):
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No product data provided"}), 400
+    
+    update_payload = {}
+
+    for field in data.keys():
+        update_payload[field] = data.get(field)
+
+    product_ref = db.collection("product").document(product_id)
+    
+    try:
+        product_ref.update(update_payload)
+        return jsonify({"message": "Product information updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
